c52da6dc1bc02259474a2f3438bc1889c9e3a8a8
Greg Roodt <groodt@gmail.com>
2012-02-25 08:52:16 +0000

    PEP 8 formatting.

--- twitter-oauth-streaming.py
+++ twitter-oauth-streaming.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python
 import oauth2 as oauth
-import urlparse, time, webbrowser
+import urlparse
+import time
+import webbrowser
 from twisted.internet import reactor, protocol, ssl
 from twisted.web import http
 
-CONSUMER_KEY='CHANGEME'
-CONSUMER_SECRET='CHANGEME'
-CONSUMER=oauth.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+
+CONSUMER_KEY = 'CHANGEME'
+CONSUMER_SECRET = 'CHANGEME'
+CONSUMER = oauth.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
 
 ACCESS_TOKEN_FILE = 'OAUTH_ACCESS_TOKEN'
 
@@ -16,6 +19,7 @@ TWITTER_AUTHORIZE_URL = 'http://twitter.com/oauth/authorize'
 TWITTER_STREAM_API_HOST = 'stream.twitter.com'
 TWITTER_STREAM_API_PATH = '/1/statuses/sample.json'
 
+
 class TwitterStreamer(http.HTTPClient):
     def connectionMade(self):
         self.sendCommand('GET', self.factory.url)
@@ -23,47 +27,51 @@ class TwitterStreamer(http.HTTPClient):
         self.sendHeader('User-Agent', self.factory.agent)
         self.sendHeader('Authorization', self.factory.oauth_header)
         self.endHeaders()
-  
+
     def handleStatus(self, version, status, message):
         if status != '200':
             self.factory.tweetError(ValueError("bad status"))
-  
+
     def lineReceived(self, line):
         self.factory.tweetReceived(line)
 
     def connectionLost(self, reason):
         self.factory.tweetError(reason)
 
+
 class TwitterStreamerFactory(protocol.ClientFactory):
-      protocol = TwitterStreamer
-  
-      def __init__(self, oauth_header):
-          self.url = TWITTER_STREAM_API_PATH
-          self.agent='Twisted/TwitterStreamer'
-          self.host = TWITTER_STREAM_API_HOST
-          self.oauth_header = oauth_header
-  
-      def clientConnectionFailed(self, _, reason):
-          self.tweetError(reason)
-  
-      def tweetReceived(self, tweet):
+    protocol = TwitterStreamer
+
+    def __init__(self, oauth_header):
+        self.url = TWITTER_STREAM_API_PATH
+        self.agent = 'Twisted/TwitterStreamer'
+        self.host = TWITTER_STREAM_API_HOST
+        self.oauth_header = oauth_header
+
+    def clientConnectionFailed(self, _, reason):
+        self.tweetError(reason)
+
+    def tweetReceived(self, tweet):
         print tweet
-  
-      def tweetError(self, error):
+
+    def tweetError(self, error):
         print error
+
 
 def save_access_token(key, secret):
     with open(ACCESS_TOKEN_FILE, 'w') as f:
         f.write("ACCESS_KEY=%s\n" % key)
         f.write("ACCESS_SECRET=%s\n" % secret)
 
+
 def load_access_token():
     with open(ACCESS_TOKEN_FILE) as f:
         lines = f.readlines()
 
-    str_key=lines[0].strip().split('=')[1]
-    str_secret=lines[1].strip().split('=')[1]
+    str_key = lines[0].strip().split('=')[1]
+    str_secret = lines[1].strip().split('=')[1]
     return oauth.Token(key=str_key, secret=str_secret)
+
 
 def fetch_access_token():
     client = oauth.Client(CONSUMER)
@@ -83,7 +91,7 @@ def fetch_access_token():
     print "Go to the following link in your browser:"
     print auth_url
     pin = raw_input('What is the PIN? ')
-    token = oauth.Token(request_token['oauth_token'],request_token['oauth_token_secret'])
+    token = oauth.Token(request_token['oauth_token'], request_token['oauth_token_secret'])
     token.set_verifier(pin)
 
     # Step 3: Get access token.
@@ -96,6 +104,7 @@ def fetch_access_token():
     print "     oauth_token        = %s" % request_token['oauth_token']
     print "     oauth_token_secret = %s" % request_token['oauth_token_secret']
     return (access_token['oauth_token'], access_token['oauth_token_secret'])
+
 
 def build_authorization_header(access_token):
     url = "https://%s%s" % (TWITTER_STREAM_API_HOST, TWITTER_STREAM_API_PATH)
